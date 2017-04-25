be68cbf638188b1ee0dca99864bf2ec4039bc3ef
methodist <yanagih@gmail.com>
2017-04-25 15:47:03 +0900

    change to license free wav file

--- ui/ibm/music_player.js
+++ ui/ibm/music_player.js
@@ -3,19 +3,19 @@ var audioObj = new Audio();
 function start_music(type){
     var url = "audio/jazz.wav";
     if ( type == "ジャズ" ) {
-       url = "audio/Take_Five.wav";
+       url = "audio/m_jazz.wav";
     }
     if ( type == "ロック" ) {
-       url = "audio/Unusual_Habitat.wav";
+       url = "audio/m_rock.wav";
     }
     if ( type == "クラシック" ) {
-       url = "audio/Morzart.wav";
+       url = "audio/m_classic.wav";
     }
     if ( type == "ポップス" ) {
        url = "audio/drive_sound.wav";
     }
     if ( type == "演歌" ) {
-       url = "audio/kawanonagare.wav";
+       url = "audio/m_enka.wav";
     }
     audioObj.pause();
     audioObj = new Audio(url);
